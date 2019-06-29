61a79aa75708a2997a291de2e4f4c1682c3a16be
GhostCat <1169518718@qq.com>
2019-06-30 00:51:37 +0800

    学习笔记 加上 配置文件

---
+++ server/dbs/config.js
@@ -0,0 +1,29 @@
+// 数据库配置文件
+
+export default {
+	dbs: 'mongodb://127.0.0.1:27017/student',
+	redis:{
+		get host(){
+			return '127.0.0.1'
+		},
+		get port(){
+			return 6379
+		}
+	},
+	smtp:{
+		get host(){
+			return 'smtp.qq.com'
+		},
+		get user(){
+			return '1169518718@qq.com'
+		},
+		get pass(){
+			return '授权码'
+		}
+	},
+	get code(){
+		return ()=>{
+			return Math.rendom().toString(16).slice(2,6).toUpperCase()
+		}
+	}
+}
